abd86bd82e62be3a106d4600a3390e6698aa5aa6
mickaelsantos <mickaelfsantos@gmail.com>
2019-12-11 16:47:42 +0000

    Proxima funcionalidade: layout da grelha

--- app/src/main/java/com/example/sudoku/MainActivity.java
+++ app/src/main/java/com/example/sudoku/MainActivity.java
@@ -3,6 +3,7 @@ package com.example.sudoku;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +11,17 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void jogar(View view) {
+        System.out.println("ola");
+    }
+
+    public void jogarMultiplayer(View view) {
+        System.out.println("ola");
+    }
+
+    public void entrar(View view) {
+        System.out.println("ola");
     }
 }
