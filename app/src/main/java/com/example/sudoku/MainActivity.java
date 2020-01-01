0e5f6d757cb1dd89ce50b1a83a76428627629de2
André Filipe Melo <melo.andre.filipe@gmail.com>
2020-01-01 22:19:03 +0000

    Forçar o perfil no primeiro boot, permissoes

--- app/src/main/java/com/example/sudoku/MainActivity.java
+++ app/src/main/java/com/example/sudoku/MainActivity.java
@@ -9,50 +9,70 @@ import android.view.View;
 import android.widget.Button;
 
 import com.example.sudoku.Core.AppDatabase;
+import com.example.sudoku.Core.Player;
+
+import java.util.List;
 
 
 public class MainActivity extends AppCompatActivity {
-    public static AppDatabase appDatabase;
-    private Button btnJogar, btnProfile, btnHistorico;
+        public static AppDatabase appDatabase;
+        private Button btnJogar, btnProfile, btnHistorico;
+        private Player player;
 
-    @Override
-    public void onBackPressed() {
-        finish();
-    }
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        appDatabase = Room.databaseBuilder(getApplicationContext(),AppDatabase.class, "database").build();
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main_menu);
-
-        btnJogar = findViewById(R.id.btnJogar);
-        btnProfile = findViewById(R.id.btnProfile);
-        btnHistorico = findViewById(R.id.btnHistorico);
-
-        btnJogar.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent intent = new Intent(MainActivity.this, Jogar.class);
-                startActivity(intent);
-                overridePendingTransition(R.anim.slide_right,R.anim.slide_out_left);
+        @Override
+        public void onBackPressed() {
                 finish();
-            }
-        });
+        }
 
-        btnProfile.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent intent = new Intent(MainActivity.this,  Profile.class);
-                startActivity(intent);
-            }
-        });
-        btnHistorico.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent intent = new Intent(MainActivity.this,  Historico.class);
-                startActivity(intent);
-            }
-        });
+        @Override
+        protected void onCreate(Bundle savedInstanceState) {
+                appDatabase = Room.databaseBuilder(getApplicationContext(), AppDatabase.class, "database").build();
+                super.onCreate(savedInstanceState);
+                setContentView(R.layout.activity_main_menu);
 
-    }
+                btnJogar = findViewById(R.id.btnJogar);
+                btnProfile = findViewById(R.id.btnProfile);
+                btnHistorico = findViewById(R.id.btnHistorico);
+
+                Thread thread = new Thread() {
+                        @Override
+                        public void run() {
+                                List<Player> players = MainActivity.appDatabase.player().getPlayer();
+                                if (players.isEmpty()) {
+                                        Intent intent = new Intent(MainActivity.this, Profile.class);
+                                        startActivity(intent);
+                                        finish();
+                                }
+                        }
+                };
+                thread.start();
+
+                btnJogar.setOnClickListener(new View.OnClickListener() {
+                        @Override
+                        public void onClick(View v) {
+                                Intent intent = new Intent(MainActivity.this, Jogar.class);
+                                startActivity(intent);
+                                overridePendingTransition(R.anim.slide_right, R.anim.slide_out_left);
+                                finish();
+                        }
+                });
+
+                btnProfile.setOnClickListener(new View.OnClickListener() {
+                        @Override
+                        public void onClick(View v) {
+                                Intent intent = new Intent(MainActivity.this, Profile.class);
+                                startActivity(intent);
+                                finish();
+                        }
+                });
+                btnHistorico.setOnClickListener(new View.OnClickListener() {
+                        @Override
+                        public void onClick(View v) {
+                                Intent intent = new Intent(MainActivity.this, Historico.class);
+                                startActivity(intent);
+                                finish();
+                        }
+                });
+
+        }
 }
--- app/src/main/java/com/example/sudoku/Profile.java
+++ app/src/main/java/com/example/sudoku/Profile.java
@@ -1,29 +1,29 @@
 package com.example.sudoku;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.room.Room;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
 
+import android.Manifest;
 import android.app.Activity;
 import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
-import android.graphics.Camera;
 import android.graphics.Matrix;
 import android.os.Bundle;
-import android.util.Base64;
 import android.view.View;
 import android.view.WindowManager;
 import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.Toast;
-
-import com.example.sudoku.Core.AppDatabase;
 import com.example.sudoku.Core.Player;
 import com.google.android.material.textfield.TextInputEditText;
 
 import java.io.File;
-import java.io.IOException;
-import java.nio.file.Files;
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 import java.util.Objects;
 
@@ -35,7 +35,9 @@ public class Profile extends AppCompatActivity {
         Player player;
         String path;
         private static final int PHOTO_REQUEST_CODE = 0;
-
+        private final static int REQUEST_CODE_ASK_PERMISSIONS = 1;
+        private static final String[] REQUIRED_SDK_PERMISSIONS = new String[] {
+            Manifest.permission.INTERNET, Manifest.permission.WRITE_EXTERNAL_STORAGE , Manifest.permission.CAMERA};
         @Override
         public void onBackPressed() {
                 if (player != null) {
@@ -50,10 +52,9 @@ public class Profile extends AppCompatActivity {
         @Override
         protected void onCreate(Bundle savedInstanceState) {
                 super.onCreate(savedInstanceState);
+                checkPermissions();
                 setContentView(R.layout.activity_profile);
                 getWindow().setSoftInputMode(WindowManager.LayoutParams.SOFT_INPUT_STATE_VISIBLE | WindowManager.LayoutParams.SOFT_INPUT_ADJUST_RESIZE);
-
-
                 btnPhoto = findViewById(R.id.btnPhoto);
                 photo = findViewById(R.id.ivPhoto);
                 btnCancel = findViewById(R.id.btnCancel);
@@ -64,22 +65,24 @@ public class Profile extends AppCompatActivity {
                         @Override
                         public void run() {
                                 List<Player> players = MainActivity.appDatabase.player().getPlayer();
-                                player = players.get(0);
-                                Profile.this.runOnUiThread(new Runnable() {
-                                        public void run() {
-                                                tfName.setText(player.getName());
-                                                File photoFile = new File(player.getPhotoPath());
+                                if (!players.isEmpty()) {
+                                        player = players.get(0);
+                                        Profile.this.runOnUiThread(new Runnable() {
+                                                public void run() {
+                                                        tfName.setText(player.getName());
+                                                        File photoFile = new File(player.getPhotoPath());
 
-                                                if (photoFile.exists()) {
-                                                        Bitmap myBitmap = BitmapFactory.decodeFile(photoFile.getAbsolutePath());
-                                                        float degrees = -90;
-                                                        Matrix matrix = new Matrix();
-                                                        matrix.setRotate(degrees);
-                                                        Bitmap bOutput = Bitmap.createBitmap(myBitmap, 0, 0, myBitmap.getWidth(), myBitmap.getHeight(), matrix, true);
-                                                        photo.setImageBitmap(bOutput);
+                                                        if (photoFile.exists()) {
+                                                                Bitmap myBitmap = BitmapFactory.decodeFile(photoFile.getAbsolutePath());
+                                                                float degrees = -90;
+                                                                Matrix matrix = new Matrix();
+                                                                matrix.setRotate(degrees);
+                                                                Bitmap bOutput = Bitmap.createBitmap(myBitmap, 0, 0, myBitmap.getWidth(), myBitmap.getHeight(), matrix, true);
+                                                                photo.setImageBitmap(bOutput);
+                                                        }
                                                 }
-                                        }
-                                });
+                                        });
+                                }
                         }
                 };
                 thread.start();
@@ -158,4 +161,38 @@ public class Profile extends AppCompatActivity {
                 }
         }
 
+        protected void checkPermissions() {
+                final List<String> missingPermissions = new ArrayList<>();
+                for (final String permission : REQUIRED_SDK_PERMISSIONS) {
+                        final int result = ContextCompat.checkSelfPermission(this, permission);
+                        if (result != PackageManager.PERMISSION_GRANTED) {
+                                missingPermissions.add(permission);
+                        }
+                }
+                if (!missingPermissions.isEmpty()) {
+                        final String[] permissions = missingPermissions.toArray(new String[missingPermissions.size()]);
+                        ActivityCompat.requestPermissions(this, permissions, REQUEST_CODE_ASK_PERMISSIONS);
+                } else {
+                        final int[] grantResults = new int[REQUIRED_SDK_PERMISSIONS.length];
+                        Arrays.fill(grantResults, PackageManager.PERMISSION_GRANTED);
+                        onRequestPermissionsResult(REQUEST_CODE_ASK_PERMISSIONS, REQUIRED_SDK_PERMISSIONS, grantResults);
+                }
+        }
+
+        @Override
+        public void onRequestPermissionsResult(int requestCode, @NonNull String permissions[],
+                                               @NonNull int[] grantResults) {
+                switch (requestCode) {
+                        case REQUEST_CODE_ASK_PERMISSIONS:
+                                for (int index = permissions.length - 1; index >= 0; --index) {
+                                        if (grantResults[index] != PackageManager.PERMISSION_GRANTED) {
+                                                Toast.makeText(this, "Required permission '" + permissions[index]
+                                                    + "' not granted, exiting", Toast.LENGTH_LONG).show();
+                                                finish();
+                                                return;
+                                        }
+                                }
+                                break;
+                }
+        }
 }
